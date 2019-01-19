848ac575a7e35177f793eccc4e2af6a8b4d97db0
yashinshekh <nyasin585@gmail.com>
2019-01-19 10:09:41 +0545

    updated files

--- fiverr_splash_scrapy/spiders/app.py
+++ fiverr_splash_scrapy/spiders/app.py
@@ -5,13 +5,30 @@ from scrapy_splash import SplashRequest
 
 class AppSpider(scrapy.Spider):
     name = 'app'
-    allowed_domains = ['google.com']
-    start_urls = ['']
+    allowed_domains = ['kiezebrink.eu']
+    start_urls = ['https://kiezebrink.eu/en/wholesale/frozen-foods/']
 
+    script1 = """
+            function main(splash, args)
+            assert (splash:go(args.url))
+            assert (splash:wait(0.5))
+            return {
+                html = splash: html(),
+                png = splash:png(),
+                har = splash:har(),
+            }
+            end
+          """
 
     def start_requests(self):
         for line in self.start_urls:
-            yield SplashRequest(line,callback=self.parse,args={'wait':'5'})
+            yield SplashRequest(line,callback=self.parse,
+                                endpoint='render.html',
+                                args={
+                                    'html': 1,
+                                    'lua_source': self.script1,
+                                    'wait': 0.5,
+                                })
 
     def parse(self, response):
         pass
